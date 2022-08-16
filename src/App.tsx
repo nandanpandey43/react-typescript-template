03fa55166393666a3c7edd91a6dc3fabe9113dfa
nandan <nandankpandey@gmail.com>
2022-08-16 11:11:19 +0530

    webpack env configured for prod and development

--- src/App.tsx
+++ src/App.tsx
@@ -4,5 +4,13 @@ import "./styles.css"
 export const App = () =>  {
     return <h1>
         React Typescript Webpack Template
+        {/* <br /> */}
+        <br />
+        <h5>
+        env.NODE_ENV - {process.env.NODE_ENV}
+        </h5>
+        <h5>
+        env.name - {process.env.name}
+        </h5>
     </h1>
 }
